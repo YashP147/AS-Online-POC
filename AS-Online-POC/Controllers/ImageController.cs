24baf1eb958d721ecedd90f714c348b3d0e47c97
YashP147 <yash.poddar@kickdrumtech.com>
2024-08-09 14:37:46 +0530

    Initial commit

---
+++ AS-Online-POC/Controllers/ImageController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class ImageController : ApiController
+    {
+        [HttpGet]
+        [Route("api/image")]
+        public string Get()
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        [Route("api/image/hello")]
+        public string Hello()
+        {
+            return "hello";
+        }
+    }
+}
